4e29aff373e306153f52ba3cd994af1075e65b98
msakac <msakac@foi.hr>
2022-04-29 10:32:43 +0200

    Omogućeno klikanje na odgovore. Provjera kolko je odgovora oznaceno i oznacivanje odgovora prilikom promjene pitanja

--- js/igra.js
+++ js/igra.js
@@ -25,15 +25,13 @@ let pitanja = [
     }
 ]
 
+var dict = {}
+
 //Dodavanje slajdova u html container
 izmjesajListu()
 slajdoviContainer.innerHTML = pitanja.map(function dodajSlajdove(element, index){
     return `<a id="slajd">${++index}</a>`
 }).join('')
-
-//Prikaz prvog pitanja i odgovora
-
-prikaziPitanjeIOdgovore(1)
 
 
 //Dohvacam sve kreirane slajdove i kreiram slušače
@@ -46,6 +44,10 @@ slajdovi.forEach(slajd=>{
         promijeniPitanje(slajd)
     })
 })
+ //Prikaz prvog pitanja i odgovora
+
+ prikaziPitanjeIOdgovore(1)
+
  //Kad kliknem na novi slajd, obrise se stajl sa gumba koji je slajda koji je bil prije njega
 function obrisiStyleSaPrethodnogSlajda(prethodniSlajd){
     slajdovi.forEach(slajd=>{
@@ -73,7 +75,7 @@ function promijeniPitanje(slajd){
 function prikaziPitanjeIOdgovore(rbPitanja){
     odgovoriLijevo.innerHTML = ""
     odgovoriDesno.innerHTML = ""
-    naslovPitanje.innerHTML = `<p id="pitanje">Pitanje ${rbPitanja}</p>`
+    naslovPitanje.innerHTML = `<p id="nazivPitanja">Pitanje ${rbPitanja}</p>`
     pitanje.innerHTML = `<p id="pitanje">${pitanja[rbPitanja- 1].pitanje}</p>`
     for (let index = 0; index < pitanja[rbPitanja-1].brojOdgovora; index++) {
         if(index === 0 || index%2 === 0){
@@ -83,6 +85,10 @@ function prikaziPitanjeIOdgovore(rbPitanja){
         }
     }
     rbPitanja == 1 ? btnPrethodni.style.visibility = 'hidden' : btnPrethodni.style.visibility = 'visible'
+    rbPitanja == 4 ? btnSljedeci.style.visibility = 'hidden' : btnSljedeci.style.visibility = 'visible'
+
+    KreirajSlusaceZaOdgovore()
+    provjeriOdabraneOdgovore()
 }
 
 //Funkcija za dobivanje random broja
@@ -123,4 +129,54 @@ btnPrethodni.addEventListener('click', e=>{
         }
     });
 })
+
+//dohvacam sve odgovore i kreiram slusaca za svaki odgovor
+function KreirajSlusaceZaOdgovore(){
+    listaOdgovora = Array.from(document.querySelectorAll("#odgovor"))
+    listaOdgovora.forEach(odgovor=>{
+        odgovor.addEventListener('click', e =>{
+            dodajOdabraniOdgovor(odgovor)
+        })
+    })
+}
+
+//Funkcija dodaje odabrani odgovor u dictionary ako već ne postoji
+function dodajOdabraniOdgovor(odgovor){
+    dict[trenutniSlajd] == null ? dict[trenutniSlajd] = [] : null //ako jos nema odgovora za to pitanje, kreiram novu listu
+
+    //Ako odgovor nije odabran
+    if(dict[trenutniSlajd].find(element => element == odgovor.innerHTML) === undefined){
+        //Provjera kolko je odgovora odabrano do sad
+        console.log(dict[trenutniSlajd].length)
+        if(dict[trenutniSlajd].length >= parseInt(trenutniSlajd)+2){
+            console.log("Dosegnut maksimalni broj odgovora")
+            //TO DO prikazi alert message
+        }else{
+            dict[trenutniSlajd].push(odgovor.innerHTML)
+            odgovor.style = "background: rgba(228,155,15, 0.8)"
+        }
+        
+    }else{
+        //Ako vec postoji odgovor onda ga obrisem iz liste i promjenim style
+        console.log("Odgovor vec postoji")
+        index = dict[trenutniSlajd].indexOf(odgovor.innerHTML)
+        dict[trenutniSlajd].splice(index,1)
+        odgovor.style = "background: #673AB7"
+    }
+    console.log("Pitanje " + trenutniSlajd + " Odgovori "+  dict[trenutniSlajd])
+
+}
+ function provjeriOdabraneOdgovore(){
+     if(dict[trenutniSlajd] != null){
+        listaOdgovora.forEach(gumbOdgovora => {
+            dict[trenutniSlajd].forEach(odabraniOdgovor => {
+                gumbOdgovora.innerHTML == odabraniOdgovor ? gumbOdgovora.style = "background: rgba(228,155,15, 0.8)" : null;
+            });
+        });
+    }
+ }
+
+
+
+
 
